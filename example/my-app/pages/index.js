03949c88e648082744c29fab50610635dd2fca32
qiyisi <qiyisi@qq.com>
2023-11-08 16:15:13 +0800

    Update

--- example/my-app/pages/index.js
+++ example/my-app/pages/index.js
@@ -1,20 +1,27 @@
-import { createImage, getJobById, getJobs, init } from "@8glabs/imagine-api";
+import {
+  createImage,
+  getJobById,
+  getJobs,
+  imageToVideo,
+  init,
+} from "@8glabs/imagine-api";
 import { useState } from "react";
 
 export default function ApiTest() {
-  init("app_id", "app_key");
+  init("test", "test");
 
   const [prompt, setPromt] = useState("");
-  const [promptJobId, setPromptJobId] = useState("");
+  const [createdJobId, setCreatedJobId] = useState("");
   const [jobId, setJobId] = useState("");
   const [jobResult, setJobResult] = useState();
   const [allJobsResult, setAllJobsResult] = useState();
+  const [url, setUrl] = useState("");
 
   const handleSubmit = async () => {
     if (!prompt) return;
     try {
       const result = await createImage(prompt);
-      setPromptJobId(result.jobId);
+      setCreatedJobId(result.jobId);
     } catch {
       console.log("error");
     }
@@ -39,25 +46,62 @@ export default function ApiTest() {
     }
   };
 
+  const handleImageToVideo = async () => {
+    if (!url) return;
+    try {
+      const result = await imageToVideo(url);
+      setJobResult(result.jobId);
+    } catch {
+      console.log("error");
+    }
+  };
+
   return (
     <>
       <div>
         <div>
           <div>Create Image</div>
-          <input value={prompt} onChange={(e) => setPromt(e.target.value)} />
+          <input
+            value={prompt}
+            onChange={(e) => setPromt(e.target.value)}
+            placeholder="prompt"
+          />
           <button onClick={handleSubmit}>submit</button>
-          {promptJobId && <div>id: {promptJobId}</div>}
+          {createdJobId && <div>id: {createdJobId}</div>}
+        </div>
+        <div>
+          <div>Image To Video</div>
+          <input
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            placeholder="image url"
+          />
+          <button onClick={handleImageToVideo}>submit</button>
+          {createdJobId && <div>id: {createdJobId}</div>}
         </div>
         <br />
         <div>
           <div>Query Job</div>
-          <input value={jobId} onChange={(e) => setJobId(e.target.value)} />
+          <input
+            value={jobId}
+            onChange={(e) => setJobId(e.target.value)}
+            placeholder="job id"
+          />
           <button onClick={handleQuery}>query</button>
           {jobResult && (
             <div>
               <div>status: {jobResult.status}</div>
               {jobResult.status === "JOB_STATUS_DONE" && (
-                <img src={jobResult.medias[0].url} alt="" />
+                <>
+                  {jobResult.type === "INFERENCE" && (
+                    <img src={jobResult.medias[0].url} alt="" />
+                  )}
+                  {jobResult.type === "MULTI_IMAGE_2_VIDEO_INFERENCE" && (
+                    <video controls>
+                      <source src={jobResult.medias[0].url} type="video/mp4" />
+                    </video>
+                  )}
+                </>
               )}
             </div>
           )}
--- imagine-api/index.js
+++ imagine-api/index.js
@@ -17,10 +17,59 @@ exports.createImage = async function (prompt) {
   };
   const data = {
     job: {
-      job_type: 3,
+      type: 3,
       status: "JOB_STATUS_PENDING",
       priority: 1,
-      metadata: `{"inference_input":{"${prompt}":"elon musk","inference_params":{"num_output_imgs":3,"output_img_height":512,"output_img_width":512,"output_object_prefix":"media/original","base_model":"Deliberate"}}}`,
+      metadata: `{"inference_input":{"prompt":{"partial_prompt":"${prompt}","style_id":"","character_infos":[]},"inference_params":{"num_output_imgs":3,"output_img_height":512,"output_img_width":512,"output_object_prefix":"media/original","base_model":"8glabs/Deliberate"}}}`,
+    },
+  };
+  const response = await fetch("http://34.66.32.56:8894/v1/create_job", {
+    method: "POST",
+    headers,
+    body: JSON.stringify(data),
+  });
+  const result = await response.json();
+  return result;
+};
+
+exports.imageToVideo = async function (url) {
+  if (!app_id || !app_key || !url) return;
+  const headers = {
+    app_id: app_id,
+    app_key: app_key,
+    "Content-Type": "application/json",
+  };
+  const data = {
+    job: {
+      type: 16,
+      status: "JOB_STATUS_PENDING",
+      priority: 1,
+      metadata: `{"input":{"images":[{"end_time":4,"image_url":"${url}"}]},"inference_params":{"duration":4,"aspect_ratio":1,"audio_url":"","output_object_prefix":"api_test"}}`,
+    },
+  };
+  const response = await fetch("http://34.66.32.56:8894/v1/create_job", {
+    method: "POST",
+    headers,
+    body: JSON.stringify(data),
+  });
+  const result = await response.json();
+  return result;
+};
+
+exports.videoToVideo = async function (prompt, videoUrl, imgUrl, duration) {
+  if (!app_id || !app_key || !prompt || !videoUrl || !imgUrl || !duration)
+    return;
+  const headers = {
+    app_id: app_id,
+    app_key: app_key,
+    "Content-Type": "application/json",
+  };
+  const data = {
+    job: {
+      type: 9,
+      status: "JOB_STATUS_PENDING",
+      priority: 1,
+      metadata: `{"structured_prompt":{"style_id":"","partial_prompt":"${prompt}","character_infos":[]},"inference_params":{"type":2,"duration":${duration},"watermark_add":false,"user_input_media_url":"${videoUrl}","system_generated_input_media_id":"4d32b14a-e2b8-47c9-9e1b-698f13fe165c","system_generated_input_media_url":"${imgUrl}","transform":6,"rendering_model":null,"base_model":"8glabs/Deliberate"}}`,
     },
   };
   const response = await fetch("http://34.66.32.56:8894/v1/create_job", {
